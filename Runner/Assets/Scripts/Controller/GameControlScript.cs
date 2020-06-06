aee5fee8da3c969fb64e78e1d2dab94d0cbcbba6
Adii020 <adrianswidere8@gmail.com>
2020-06-06 16:35:05 +0200

    Runner - gracz musi omijać przeszkody które się generują. Dodane są skrypty odpowiedzialny za zmianę toru. Skrypt generujący przeszkody. Skrypt GameControlScript sprawdza czy gra się toczy., czy pasek basansu nie przepełnił się. Dodany jest efekt zbierania punktów.

---
+++ Runner/Assets/Scripts/Controller/GameControlScript.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+ 
+
+public class GameControlScript : MonoBehaviour {
+	
+	public float objectSpeed = 0;
+	
+	
+	// Czy gra nadal się toczy?
+	bool isRunning = true;
+	int playerScore = 0;
+	
+
+	//Sound
+	public AudioClip SoundToPlay;
+	private float Volume = 1;
+	AudioSource audio;
+
+	public Text TextScore;
+
+	//Menu
+	public GameObject Menu;
+	private GameObject Playbutton;
+	private GameObject Resetbutton;
+	private GameObject Quitbutton;
+
+
+	//Progres bar
+	public Image collectBar;
+
+	// zmienna wypełniająca pasek gdy gracz jest na wyznaczonym torze
+	public float timeAmt = 100f;
+	float time;
+
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 0f;
+		TextScore = GameObject.Find("GUI/ScorePanel/Text").GetComponent<Text>();
+		Menu = GameObject.Find("GUI/Menu");	
+		Playbutton = GameObject.Find("GUI/Menu/Play");
+		Quitbutton = GameObject.Find("GUI/Menu/Quit");
+		Resetbutton = GameObject.Find("GUI/Menu/Reset");
+		Playbutton.SetActive(true);
+		Quitbutton.SetActive(true);
+		Resetbutton.SetActive(false);
+		Playbutton.GetComponent<Button>().onClick.AddListener(PlayGame);
+		Resetbutton.GetComponent<Button>().onClick.AddListener(ResetGame);
+		Quitbutton.GetComponent<Button>().onClick.AddListener(QuitGame);
+
+		collectBar = GameObject.Find("GUI/BarPanel/PointBarCol1/PointBarCol2").GetComponent<Image>();
+		collectBar.fillAmount = 0.5f;
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		checkGameover();
+
+	}
+
+
+	// sprawdzaie czy gracz przegrał przez zapełnienie paska balansu
+	void checkGameover()
+    {
+		if (collectBar.fillAmount == 0f || collectBar.fillAmount == 1f)
+		{
+			PlayerDied();
+		}
+	}
+
+	void PlayGame()
+	{
+		//Application.LoadLevel("Level1");
+		isRunning = true;
+		Time.timeScale = 1.0f;
+		objectSpeed = -0.6f;
+		Menu.SetActive(false);
+	}
+
+	void ResetGame()
+	{
+		SceneManager.LoadScene("Level1");
+	}
+
+	void QuitGame()
+    {
+		Application.Quit();
+		Debug.Log("Game is exiting");
+	}
+
+    public void TimeAdd()
+    {
+		time = Time.deltaTime;
+		collectBar.fillAmount -= time / timeAmt;
+
+	}
+
+	public void TimeSub()
+	{
+		time = Time.deltaTime;
+		collectBar.fillAmount += time / timeAmt;
+		
+	}
+
+	//Dodawanie puntków wywoływane przez PlayerControler gdy gracz dotkie żółtego punktu
+	public void AddScore()
+	{
+		playerScore++;
+		//Debug.Log(playerScore);
+		TextScore.text = "Punkty: " + playerScore.ToString();
+	}
+
+
+	public void PlayerDied()
+	{
+		Playbutton.SetActive(false);
+		Quitbutton.SetActive(true);
+		Resetbutton.SetActive(true);
+		isRunning = false;
+		Time.timeScale = 0f;
+		objectSpeed = 0f;
+		Menu.SetActive(true);
+	}
+
+	// Dźwięk zebrania przedmiotu
+	public void PlayCollectCoinSound()
+	{
+		GetComponent<AudioSource>().PlayOneShot(SoundToPlay, Volume);
+	}
+
+	
+}
---
+++ Runner/Assets/Scripts/Controller/SpawnScript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour
+{
+	public GameObject obstacle;
+	public GameObject powerup;
+	public GameObject powerup2;
+	float timeElapsed = 0;
+	float spawnCycle = .3f;
+	bool spawnPowerup = true;
+	bool spawnPowerup2 = true;
+	public System.Random random = new System.Random();
+	public ArrayList array = new ArrayList();
+	
+	// Use this for initialization
+	void Start()
+	{
+		
+		array.Add(-2f);
+		array.Add(0f);
+		array.Add(2f);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		timeElapsed += Time.deltaTime;
+		if (timeElapsed > spawnCycle)
+		{
+			GameObject temp;
+			int objnumber = Random.Range(1, 4);
+			//Debug.Log(objnumber);
+			if (objnumber == 1)
+			{
+				temp = (GameObject)Instantiate(powerup);
+				//-2,0,2
+				Vector3 pos = temp.transform.position;
+				temp.transform.position = new Vector3((float)array[(random.Next(0, array.Count))], pos.y, pos.z);
+
+			}
+			else if (objnumber == 2)
+			{
+				temp = (GameObject)Instantiate(obstacle);
+				Vector3 pos = temp.transform.position;
+				temp.transform.position = new Vector3((float)array[(random.Next(0, array.Count))], pos.y, pos.z);
+			}
+			/*else if (objnumber == 3)
+			{
+				temp = (GameObject)Instantiate(powerup2);
+				//-2,0,2
+				Vector3 pos = temp.transform.position;
+				temp.transform.position = new Vector3((float)array[(random.Next(0, array.Count))], pos.y, pos.z);
+
+
+			}*/
+
+
+			timeElapsed -= spawnCycle;
+			spawnPowerup = !spawnPowerup;
+		}
+
+	}
+}
